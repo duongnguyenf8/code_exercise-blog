3bc9882da7b16698cd04086b81411db19d11d347
Duong Nguyen <nguyenduong@fullstack.edu.vn>
2023-10-23 22:15:44 +0700

    re-merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
 import Layout from '@/layout';
 import Router from '@/routes';
 import './assets/styles/styles.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Router />
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Router />
+    </Layout>
   );
 }
 
